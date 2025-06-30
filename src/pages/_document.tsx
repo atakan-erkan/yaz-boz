0cb44e26be6befd2fbc9b51484add21e2df4f58c
Atakan Erkan <dev.atakan.erkan@gmail.com>
2025-06-30 17:32:17 +0300

    favicon added and bug fixed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,23 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="tr">
+      <Head>
+        <meta name="description" content="Okey, Pişti, Batak, Cezali 101 oyunları için skor tabloları" />
+        <meta name="keywords" content="Okey, Pişti, Batak, Cezali 101, oyun, skor, tablo" />
+        <meta name="author" content="Okey, Pişti, Batak, Cezali 101" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="google" content="notranslate" />
+        <meta name="theme-color" content="#F5E9DA" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const saveToStorage = (key: string, value: any) => {
     if (typeof window !== "undefined") {
         localStorage.setItem(key, JSON.stringify(value));
